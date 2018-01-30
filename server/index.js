86de556e2758d355c2a54b6d286303f7b3288c89
Ubuntu <ubuntu@ip-172-31-22-210.ap-south-1.compute.internal>
2018-01-30 13:00:54 +0000

    EBM Closed - Farewell, EBM Els

--- server/index.js
+++ server/index.js
@@ -2,11 +2,7 @@
     module.exports = function (app) {
     	var path = require('path');
         app.use('/api/web',require('./routes/registrationRoutes'));
-        app.get('/registerEBM/:id',function(req,res)
-        {
-        	res.sendFile(path.resolve(__dirname,'../ebmregister.html'));
-        	console.log();
-        });
+        
         app.get('*',function(req,res)
     {
         res.redirect('/');
